85c64261a898faced0e472f77262b5d51f0f0c9c
kristinastodder <30780783+kristinastodder@users.noreply.github.com>
2019-05-14 09:32:06 -0700

    Update GPS Coordinate Extractor.py

--- GPS Coordinate Extractor.py
+++ GPS Coordinate Extractor.py
@@ -6,35 +6,33 @@ import openpyxl, pyperclip, requests, os, bs4
 from selenium import webdriver
 import time
 
+#Open Firefox browser and latlong.net
+driver = webdriver.Firefox()
+driver.get('http://latlong.net')
 
 # Open the spreadsheet and copy the value
-wb = openpyxl.load_workbook('shark2.xlsx')
+wb = openpyxl.load_workbook('California.xlsx')
 sheet = wb['Sheet1']
 for r in range(2, sheet.max_row + 1):
     location = sheet.cell(row=r, column=7).value
     pyperclip.copy(location)
 
-    #Open Firefox browser and submit location
-    driver = webdriver.Firefox()
-    driver.get('http://latlong.net')
+    #Submit location
     placeElem = driver.find_element_by_id('place')
     placeElem.send_keys(location)
     placeElem.submit()
     time.sleep(5)
 
-    #Copy lat & long to clipboard
+    #Copy lat & long to clipboard and print
     latlng = driver.find_element_by_id('latlngspan')
     pyperclip.copy(latlng.text)
-
-    #Paste into spreadsheet
-    print(location, latlng.text)
+    print(latlng.text)
 
     #Browser Back
     driver.back()
     
-    #Close geckodriver
-    #Separate latitude & longitude?
-
+elif location = ""
+    prin("Value is null")
 
 
 
